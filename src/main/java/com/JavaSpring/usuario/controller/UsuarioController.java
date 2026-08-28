3d741d35a17d6caf33ffbcf2f4bdc7ab8aa84259
Samuel Owen <owenmacedo05@gmail.com>
2026-08-28 15:41:34 -0300

    Adicionando metodos de delete, get

--- src/main/java/com/JavaSpring/usuario/controller/UsuarioController.java
+++ src/main/java/com/JavaSpring/usuario/controller/UsuarioController.java
@@ -4,8 +4,12 @@ package com.JavaSpring.usuario.controller;
 import com.JavaSpring.usuario.business.UsuarioService;
 import com.JavaSpring.usuario.dto.UsuarioDTO;
 import com.JavaSpring.usuario.infracture.entidy.Usuario;
+import com.JavaSpring.usuario.infracture.security.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -14,10 +18,24 @@ import org.springframework.web.bind.annotation.*;
 public class UsuarioController {
 
     private final UsuarioService usuarioService;
+    private final AuthenticationManager authenticationManager;
+    private final JwtUtil jwtUtil;
 
 
+    @PostMapping
     public ResponseEntity<UsuarioDTO> salvarUsuario(@RequestBody UsuarioDTO usuarioDTO){
         return ResponseEntity.ok(usuarioService.salvarUsuario(usuarioDTO));
+    }
+    @PostMapping("/login")
+    public String login(@RequestBody UsuarioDTO usuarioDTO){
+        Authentication authentication = authenticationManager.authenticate(
+
+                new UsernamePasswordAuthenticationToken(usuarioDTO.getEmail(),
+                        usuarioDTO.getEmail())
+        );
+
+        return "Bearer " + jwtUtil.generateToken(authentication.getName());
+
     }
 
     @DeleteMapping("/{email}")
